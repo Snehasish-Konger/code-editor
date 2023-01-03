0598652f29abe6e6c1ab2bce588a04313ab2a343
Snehasish Konger <snehasish.konger.it.2020@tint.edu.in>
2023-01-03 19:45:42 +0530

    Getting ready

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="fixed h-8 bottom-0 px-2 py-1 flex items-center justify-center w-full text-xs text-gray-500 ">
+      <span>
+        Made with{" "}
+        <span role="img" aria-label="heart">
+          ❤️
+        </span>{" "}
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/LanguagesDropdown.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Select from "react-select";
+import { customStyles } from "../constants/customStyles";
+import { lang } from "../constants/lang";
+
+const LanguagesDropdown = ({ onSelectChange }) => {
+  return (
+    <Select
+      placeholder={`Filter By Category`}
+      options={lang}
+      styles={customStyles}
+      defaultValue={lang[0]}
+      onChange={(selectedOption) => onSelectChange(selectedOption)}
+    />
+  );
+};
+
+export default LanguagesDropdown;
